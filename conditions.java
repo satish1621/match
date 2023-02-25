1e0a3f755d290f5ba6a23b247fd93bbb006c7a0a
satish1621 <126313657+satish1621@users.noreply.github.com>
2023-02-25 12:08:07 +0530

    Add files via upload

---
+++ conditions.java
@@ -0,0 +1,21 @@
+class main
+{
+public static void main(String args[])
+{
+int age=15;
+int w=26;
+if(age>18)
+{
+System.out.println("he is suitable");
+if(w>25){
+
+System.out.println("he is healthy");
+System.out.println("we can take his blood");
+}
+}
+else
+{
+System.out.println("fail");
+}
+}
+}
